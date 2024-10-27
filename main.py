bb6232698655106f26d504516cb6462e43575a1e
Meghan Renshaw <meghanrenshaw@gmail.com>
2024-10-27 11:59:16 -0500

    updated main

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import hashlib
+import sys
+import os
+from dotenv import load_dotenv
+
+def main():
+    file_path = sys.argv[1]
+    id = hash_file(file_path)
+    url = f"https://www.virustotal.com/api/v3/files/{id}"
+
+    headers = { "x-apikey": "api_key"}
+
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+
+def hash_file(file_path):
+    hash = hashlib.sha256()
+
+    with open(file_path, "rb") as f:
+        file_content = f.read()
+        hash.update(file_content)
+
+    return hash.hexdigest()
+
+if __name__ == '__main__':
+    main()
+
+
